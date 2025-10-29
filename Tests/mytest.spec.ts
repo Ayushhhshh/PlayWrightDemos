e6b888abb487f5792c4f3c9b64ed15afd30a043a
Ayush Jain <2022bcacsbayush12896@poornima.edu.in>
2025-10-30 02:48:39 +0530

    More test added

--- Tests/mytest.spec.ts
+++ Tests/mytest.spec.ts
@@ -12,3 +12,13 @@ test("Verify Page Title",async  ({page})=>{
     
     await expect(page).toHaveTitle("Google");
 })
+
+test("Verify Page URL",async  ({page})=>{
+ 
+    await page.goto("https://www.google.com");
+    
+    let URL : string = await page.url();
+    console.log("URL is",URL);
+    
+    await expect(page).toHaveURL(URL);
+})
