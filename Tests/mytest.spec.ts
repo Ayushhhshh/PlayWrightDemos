cd26f42f2caa6eaeea375554c1e1ae248915442f
Ayush Jain <2022bcacsbayush12896@poornima.edu.in>
2025-10-30 02:38:14 +0530

    Test added

--- Tests/mytest.spec.ts
+++ Tests/mytest.spec.ts
@@ -8,7 +8,7 @@ test("Verify Page Title",async  ({page})=>{
     await page.goto("https://www.google.com");
     
     let title : string = await page.title();
-   console.log("Title is",title);
+    console.log("Title is",title);
     
-    expect(page).toHaveTitle("Google");
-}) 
+    await expect(page).toHaveTitle("Google");
+})
