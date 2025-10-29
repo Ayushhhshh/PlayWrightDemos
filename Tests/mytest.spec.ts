e3e2874a132d59c695818c2c2884f70086ce6857
Ayush Jain <2022bcacsbayush12896@poornima.edu.in>
2025-10-30 01:53:10 +0530

    Building

--- Tests/mytest.spec.ts
+++ Tests/mytest.spec.ts
@@ -1,9 +1,14 @@
 import {test,expect} from "@playwright/test"
+import { title } from "process";
 
 
 
-test("Verify Page Title", ({page})=>{
+test("Verify Page Title",async  ({page})=>{
  
-    page.goto("https://www.google.com");
+    await page.goto("https://www.google.com");
+    
+    let title : string = await page.title();
+   console.log("Title is",title);
+    
     expect(page).toHaveTitle("Google");
-})
+}) 
