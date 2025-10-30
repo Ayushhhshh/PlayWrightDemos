822bf3d13271cfbb1e5b90a6b6f21dc3952076d1
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2025-10-31 02:31:54 +0530

    Updated Config File and enhanced framework

--- Tests/mytest.spec.ts
+++ Tests/mytest.spec.ts
@@ -4,10 +4,10 @@ import { title } from "process";
 
 test("Verify Page Title",async  ({page})=>{
  
-    await page.goto("https://www.google.com");
+    await page.goto("https://www.youtube.com/");
     
     let title : string = await page.title();
     console.log("Title is",title);
     
-    await expect(page).toHaveTitle("Google");
+    await expect(page).toHaveTitle("Youtube");
 })
